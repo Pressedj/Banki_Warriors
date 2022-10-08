96a3b2ae46354c373bb2fbcbda53fa386f3e4c83
Juniper <jpress.box@gmail.com>
2022-10-08 09:33:07 -0400

    Working on server, runs on 3000 testing html,

--- server.js
+++ server.js
@@ -10,21 +10,30 @@ const adminBehavioralRoute = require("./routes/adminBehavioral");
 const adminTechnicalRoute = require("./routes/adminTechnical");
 
 require("dotenv").config({ path: "./config/.env" });
-connectDB();
+//connectDB();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/", homeRoute);
-app.use("/admin", adminRoute);
-app.use("/user", userRoute);
-app.use("/behavioral", behavioralRoute);
-app.use("/technical", technicalRoute);
-app.use("/admin/behavioral", adminBehavioralRoute);
-app.use("/admin/technical", adminTechnicalRoute);
+//app.use("/", homeRoute);
+//app.use("/admin", adminRoute);
+//app.use("/user", userRoute);
+//app.use("/behavioral", behavioralRoute);
+//app.use("/technical", technicalRoute);
+//app.use("/admin/behavioral", adminBehavioralRoute);
+//app.use("/admin/technical", adminTechnicalRoute);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running, you better catch it!");
-});
+//app.listen(process.env.PORT, () => {
+//  console.log("Server is running, you better catch it!");
+//});
+
+
+app.listen(3000, function(){
+    console.log('Welcome to the Banki Dojo')
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
